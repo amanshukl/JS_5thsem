753022f4a4cfb5a17842f0abb165bfd08887ef44
amanshukl <amanshuk916@gmail.com>
2025-09-16 15:24:57 +0530

    message

---
+++ day1.js
@@ -0,0 +1,27 @@
+//output method
+console.log("FSD Class");
+
+//input method
+// let a = prompt("Enter a number");
+// console.log(a);
+
+// var a = 20;
+//  var a = 30;
+
+//  let b = 40;
+//  console.log("after first assignmant  b's value :"+b);
+//  b = 70;
+//     console.log("after second assignmant  b's value :"+b);
+//      {
+//          let name = "Aman";
+//          name = "xyz";
+//          console.log("name :" + name) 
+//          b = 60;
+//          console.log("after reassinment of b's value ; ")       
+//     }
+
+let firstName = "Aman";
+let lastName = "shukla";
+let message = "Hello";
+let outputMessage = message + "," + firstName + " " + lastName + "!";
+console.log(outputMessage);
---
+++ day2.js
@@ -0,0 +1,27 @@
+// console.log(a,"a:before invokation ");
+// var a = 20;
+// console.log(message(),"invokatoion of function");
+
+// function message(){
+//     console.log("Hello Class");
+// }
+// {
+//     var a = 50;
+//     console.log(a);
+
+// }
+// console.log(a);
+
+
+//for loop(;;)
+// for(let i=1;i<=6;i = i+2){
+
+// }
+
+// function fun(){
+//     console.log("FSD Class!");
+// }
+// console.log(fun);
+// console.log(fun());
+
+
